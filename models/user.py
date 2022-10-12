43908fba765be31765f31302c6b5cb7ef3a316e5
John Agbo <Jonchukwuma04@gmail.com>
2022-10-12 22:55:37 +0000

    This file is for the User Model

--- models/user.py
+++ models/user.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Creates the user class for AirBnB"""
+"""Creates the user class for AirBnB."""
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.orm import relationship
