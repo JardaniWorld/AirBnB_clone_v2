33c31ad6022613dba1db0a49da3baa58ef6f00ac
John Agbo <Jonchukwuma04@gmail.com>
2022-10-12 22:57:25 +0000

    This file is for the amenity class

--- models/amenity.py
+++ models/amenity.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""This is the amenity class"""
+"""Creates the amenity class."""
 from models.base_model import BaseModel, Base
 from models.place import place_amenity
 from sqlalchemy import Column, String
